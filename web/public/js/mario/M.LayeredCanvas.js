a7ed49d7678f6270e1fbf3ea036a2c62ec0cd401
Andrey Balabokhin <abalabokhin@gmail.com>
2011-05-11 14:56:02 +0700

    Removed an unused code.

--- web/public/js/mario/M.LayeredCanvas.js
+++ web/public/js/mario/M.LayeredCanvas.js
@@ -1,18 +1,12 @@
 M = window.M || {};
 M.LayeredCanvas = new Class({
-    Implements : [Options,Events],
-    
-    options : {
-        width : 400,
-        height : 400,
-        parent : $(document.body)
-    },
 
+    options : {},
     layers : {},
     appDiv : {},
 
     initialize : function (options) {
-        this.setOptions(options);
+        this.options = options;
         this.appDiv = new Element('div', {
             styles: {
                 position: 'relative',
@@ -21,8 +15,7 @@ M.LayeredCanvas = new Class({
                 overflow: 'hidden'
             }
         });
-        //appDiv.inject(parent);
-        this.appDiv.inject(this.options.parent/*$(document.body)*/);
+        this.appDiv.inject(this.options.parent);
         return this;
     },
 
@@ -51,86 +44,3 @@ M.LayeredCanvas = new Class({
     }
 
 });
-
-/*M.MarioCanvas = function(bgWidth, bgHeight, fgWidth, fgHeight)
-{
-	var appDiv = new Element('div');
-	var background= new Element('canvas', {
-		width: bgWidth,
-		height: bgHeight
-	});
-	var foreground= new Element('canvas', {
-		width: fgWidth,
-		height: fgHeight
-	});
-	var x = 0;
-	var y = 0;
-
-	var init = function()
-	{
-		appDiv.inject(document.body);
-		appDiv.setStyle('position', 'relative');
-		appDiv.setStyle('width', fgWidth);
-		appDiv.setStyle('height', fgHeight);
-		appDiv.setStyle('overflow', 'hidden');
-
-		background.inject(appDiv);
-		background.setStyle('position', 'absolute');
-
-		foreground.inject(appDiv);
-		foreground.setStyle('position', 'absolute');
-		foreground.setStyle('top', 0);
-		foreground.setStyle('left', 0);
-
-		redrawBackground();
-		setBgPosition();
-		redrawForeground();
-	}
-
-	this.move = function(deltaX, deltaY)
-	{
-		x += deltaX;
-		y += deltaY;
-		setBgPosition();
-		redrawForeground();
-	}
-
-	var setBgPosition = function()
-	{
-		background.setStyle('left', -x);
-	}
-
-	var redrawBackground = function()
-	{
-		var bgContext = background.getContext('2d');
-
-		bgContext.clearRect(0, 0, bgWidth, bgHeight);
-		bgContext.beginPath();
-		bgContext.moveTo(0,0);
-		bgContext.lineTo(bgWidth, bgHeight);
-		bgContext.moveTo(0, bgHeight / 2);
-		bgContext.lineTo(bgWidth, bgHeight * 3/2);
-		bgContext.moveTo(bgWidth/2, 0);
-		bgContext.lineTo(bgWidth * 3 /2, bgHeight);
-		bgContext.moveTo(bgWidth, 0);
-		bgContext.lineTo(0, bgHeight);
-		bgContext.stroke();
-		bgContext.closePath();
-	}
-
-	var redrawForeground = function()
-	{
-		var fgContext = foreground.getContext('2d');
-		var rectWidth = 20;
-		var rectHeight = 20;
-
-		fgContext.clearRect(0, 0, fgWidth, fgHeight);
-		fgContext.beginPath();
-		var x1 = (fgWidth - rectWidth)/2;
-		var y1 = fgHeight - y - rectHeight/2;
-		fgContext.strokeRect(x1, y1, rectWidth, rectHeight);
-		fgContext.closePath();
-	}
-
-	init();
-}*/
